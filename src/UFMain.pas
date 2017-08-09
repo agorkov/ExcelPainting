5ec7d0f8f2f6008cdbb4fa1a7026d79b1500f099
Alexey <gorkoff@gmail.com>
2017-08-09 22:46:02 +0300

    Добавил прогрес бар

--- src/UFMain.pas
+++ src/UFMain.pas
@@ -4,12 +4,13 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, StdCtrls, jpeg, ExtCtrls;
+  Dialogs, StdCtrls, jpeg, ExtCtrls, ComCtrls;
 
 type
   TFMain = class(TForm)
     Img : TImage;
     BCreateExcel : TButton;
+    ProgressBar1 : TProgressBar;
     procedure FormCanResize(Sender : TObject; var NewWidth, NewHeight : Integer; var Resize : Boolean);
     procedure BCreateExcelClick(Sender : TObject);
   private
@@ -34,25 +35,31 @@ var
   FileName : string;
   i, j : Integer;
 begin
+  ProgressBar1.Position := 0;
+  ProgressBar1.Step := 1;
+  ProgressBar1.Max := Img.Picture.Height * Img.Picture.Width;
+
   FileName := ExtractFilePath(Application.ExeName) + 'test.xlsx';
   Excel := CreateOleObject('Excel.Application');
   Excel.Workbooks.Open[FileName];
 
-  for i := 0 to Img.Height - 1 do
+  for i := 0 to Img.Picture.Height - 1 do
   begin
     Excel.Rows[i + 1].RowHeight := 3.75;
   end;
 
-  for i := 0 to Img.Width - 1 do
+  for i := 0 to Img.Picture.Width - 1 do
   begin
     Excel.Columns[i + 1].ColumnWidth := 0.42;
   end;
 
-  for i := 0 to Img.Height - 1 do
+  for i := 0 to Img.Picture.Height - 1 do
   begin
-    for j := 0 to Img.Width - 1 do
+    for j := 0 to Img.Picture.Width - 1 do
     begin
-      Excel.Cells.Item[i + 1, j + 1].Interior.Color := Img.Canvas.Pixels[j,i];
+      Excel.Cells.Item[i + 1, j + 1].Interior.Color := Img.Canvas.Pixels[j, i];
+      ProgressBar1.StepIt;
+      Application.ProcessMessages;
     end;
   end;
 
