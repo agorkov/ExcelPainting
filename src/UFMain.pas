e3b5cd0a1e7a3ae3bf78307654c8682f74f7db05
Alexey <gorkoff@gmail.com>
2017-08-09 20:32:57 +0300

    Черновая версия

--- src/UFMain.pas
+++ src/UFMain.pas
@@ -32,17 +32,35 @@ procedure TFMain.BCreateExcelClick(Sender : TObject);
 var
   Excel : Variant;
   FileName : string;
+  i, j : Integer;
 begin
   FileName := ExtractFilePath(Application.ExeName) + 'test.xlsx';
   Excel := CreateOleObject('Excel.Application');
   Excel.Workbooks.Open[FileName];
 
-  Excel.Range['a1'].ColumnWidth := 100;
-  Excel.Range['a1'].RowHeight := 100;
+  for i := 0 to Img.Height - 1 do
+  begin
+    Excel.Rows[i + 1].RowHeight := 3.75;
+  end;
+
+  for i := 0 to Img.Width - 1 do
+  begin
+    Excel.Columns[i + 1].ColumnWidth := 0.42;
+  end;
+
+  for i := 0 to Img.Height - 1 do
+  begin
+    for j := 0 to Img.Width - 1 do
+    begin
+      Excel.Cells.Item[i + 1, j + 1].Interior.Color := Img.Canvas.Pixels[j,i];
+    end;
+  end;
 
   Excel.ActiveWorkbook.Save;
   Excel.ActiveWorkbook.Close;
   Excel.Application.Quit;
+
+  Application.Terminate;
 end;
 
 procedure TFMain.FormCanResize(Sender : TObject; var NewWidth, NewHeight : Integer; var Resize : Boolean);
