4da5daa4e8098609b16616604cfcc3a329e1a65d
Alexey <gorkoff@gmail.com>
2017-08-09 19:41:41 +0300

    Пример установки размера ячейки

--- src/UFMain.pas
+++ src/UFMain.pas
@@ -4,11 +4,14 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs;
+  Dialogs, StdCtrls, jpeg, ExtCtrls;
 
 type
   TFMain = class(TForm)
+    Img : TImage;
+    BCreateExcel : TButton;
     procedure FormCanResize(Sender : TObject; var NewWidth, NewHeight : Integer; var Resize : Boolean);
+    procedure BCreateExcelClick(Sender : TObject);
   private
     { Private declarations }
   public
@@ -21,6 +24,26 @@ var
 implementation
 
 {$R *.dfm}
+
+uses
+  ComObj;
+
+procedure TFMain.BCreateExcelClick(Sender : TObject);
+var
+  Excel : Variant;
+  FileName : string;
+begin
+  FileName := ExtractFilePath(Application.ExeName) + 'test.xlsx';
+  Excel := CreateOleObject('Excel.Application');
+  Excel.Workbooks.Open[FileName];
+
+  Excel.Range['a1'].ColumnWidth := 100;
+  Excel.Range['a1'].RowHeight := 100;
+
+  Excel.ActiveWorkbook.Save;
+  Excel.ActiveWorkbook.Close;
+  Excel.Application.Quit;
+end;
 
 procedure TFMain.FormCanResize(Sender : TObject; var NewWidth, NewHeight : Integer; var Resize : Boolean);
 begin
